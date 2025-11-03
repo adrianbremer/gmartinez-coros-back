5f2f79bc5347b04ced875e64fb7376619dade87f
Adrian Bremer <adrianbremer@gmail.com>
2025-11-03 09:52:57 -0600

    Refactor server configuration and update Vite config to use module.exports

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
-    url: env('PUBLIC_URL', `http://${env('HOST', '0.0.0.0')}:${env.int('PORT', 1337)}`),
+    url: env('PUBLIC_URL'),
+    proxy: true, // Always true for Coolify deployments
     app: {
         keys: env.array('APP_KEYS'),
     },
--- src/admin/vite.config.js
+++ src/admin/vite.config.js
@@ -1,21 +1,23 @@
-import { mergeConfig } from 'vite';
-
-export default (config) => {
+module.exports = (config) => {
     // Important: always return the modified config
-    return mergeConfig(config, {
+    return {
+        ...config,
         resolve: {
+            ...config.resolve,
             alias: {
+                ...config.resolve?.alias,
                 '@': '/src',
             },
         },
         server: {
+            ...config.server,
             allowedHosts: [
                 'back.coralia.com.mx',
                 'localhost',
-                '127.0.0.1'
+                '127.0.0.1',
+                '.coralia.com.mx'
             ],
-            host: '0.0.0.0',
-            port: 1337
+            host: '0.0.0.0'
         },
-    });
+    };
 };
