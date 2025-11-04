5799496e87771be815bb22e14e820cd10bd640b5
Adrian Bremer <adrianbremer@gmail.com>
2025-11-04 13:22:21 -0600

    Consolidate attendance routes into a single file and enhance custom route configurations with authentication scopes

--- src/api/attendance/routes/attendance.js
+++ src/api/attendance/routes/attendance.js
@@ -6,4 +6,74 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::attendance.attendance');
+module.exports = {
+    routes: [
+        // Core CRUD routes
+        {
+            method: 'GET',
+            path: '/attendances',
+            handler: 'api::attendance.attendance.find',
+        },
+        {
+            method: 'GET',
+            path: '/attendances/:id',
+            handler: 'api::attendance.attendance.findOne',
+        },
+        {
+            method: 'POST',
+            path: '/attendances',
+            handler: 'api::attendance.attendance.create',
+        },
+        {
+            method: 'PUT',
+            path: '/attendances/:id',
+            handler: 'api::attendance.attendance.update',
+        },
+        {
+            method: 'DELETE',
+            path: '/attendances/:id',
+            handler: 'api::attendance.attendance.delete',
+        },
+        // Custom routes
+        {
+            method: 'POST',
+            path: '/attendances/respond',
+            handler: 'api::attendance.attendance.respondToInvitation',
+            config: {
+                auth: {
+                    scope: ['authenticated']
+                },
+            },
+        },
+        {
+            method: 'GET',
+            path: '/attendances/event/:eventId',
+            handler: 'api::attendance.attendance.getEventAttendances',
+            config: {
+                auth: {
+                    scope: ['authenticated']
+                },
+            },
+        },
+        {
+            method: 'GET',
+            path: '/attendances/my-invitations',
+            handler: 'api::attendance.attendance.getMyInvitations',
+            config: {
+                auth: {
+                    scope: ['authenticated']
+                },
+            },
+        },
+        {
+            method: 'PUT',
+            path: '/attendances/:attendanceId/check-in',
+            handler: 'api::attendance.attendance.checkIn',
+            config: {
+                auth: {
+                    scope: ['authenticated']
+                },
+            },
+        },
+    ]
+};
