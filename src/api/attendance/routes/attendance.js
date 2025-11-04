3a338aebe7b9637edf213dac39a537aec03967af
Adrian Bremer <adrianbremer@gmail.com>
2025-11-04 11:35:05 -0600

    Enhance CORS configuration and refactor attendance routes for clarity

--- src/api/attendance/routes/attendance.js
+++ src/api/attendance/routes/attendance.js
@@ -6,47 +6,53 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-const defaultRouter = createCoreRouter('api::attendance.attendance');
+// Create the core router
+const coreRouter = createCoreRouter('api::attendance.attendance');
 
-// Add custom routes to default routes
+// Custom routes
+const customRoutes = [
+    {
+        method: 'POST',
+        path: '/attendances/respond',
+        handler: 'attendance.respondToInvitation',
+        config: {
+            policies: [],
+            middlewares: [],
+        },
+    },
+    {
+        method: 'GET',
+        path: '/attendances/event/:eventId',
+        handler: 'attendance.getEventAttendances',
+        config: {
+            policies: [],
+            middlewares: [],
+        },
+    },
+    {
+        method: 'GET',
+        path: '/attendances/my-invitations',
+        handler: 'attendance.getMyInvitations',
+        config: {
+            policies: [],
+            middlewares: [],
+        },
+    },
+    {
+        method: 'PUT',
+        path: '/attendances/:attendanceId/check-in',
+        handler: 'attendance.checkIn',
+        config: {
+            policies: [],
+            middlewares: [],
+        },
+    },
+];
+
+// Export router with custom routes added
 module.exports = {
     routes: [
-        ...defaultRouter.routes,
-        {
-            method: 'POST',
-            path: '/attendances/respond',
-            handler: 'attendance.respondToInvitation',
-            config: {
-                policies: [],
-                middlewares: [],
-            },
-        },
-        {
-            method: 'GET',
-            path: '/attendances/event/:eventId',
-            handler: 'attendance.getEventAttendances',
-            config: {
-                policies: [],
-                middlewares: [],
-            },
-        },
-        {
-            method: 'GET',
-            path: '/attendances/my-invitations',
-            handler: 'attendance.getMyInvitations',
-            config: {
-                policies: [],
-                middlewares: [],
-            },
-        },
-        {
-            method: 'PUT',
-            path: '/attendances/:attendanceId/check-in',
-            handler: 'attendance.checkIn',
-            config: {
-                policies: [],
-                middlewares: [],
-            },
-        },
+        ...coreRouter.routes,
+        ...customRoutes,
     ],
 };
