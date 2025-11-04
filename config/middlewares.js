eb5ac393c98e1f334b9fa844547f88fa9e9de4b0
Adrian Bremer <adrianbremer@gmail.com>
2025-11-04 12:29:36 -0600

    Remove unnecessary CORS middleware enabled flag for cleaner configuration

--- config/middlewares.js
+++ config/middlewares.js
@@ -50,7 +50,6 @@ module.exports = [
     {
         name: 'strapi::cors',
         config: {
-            enabled: true,
             headers: '*',
             origin: [
                 'http://localhost:1337',
